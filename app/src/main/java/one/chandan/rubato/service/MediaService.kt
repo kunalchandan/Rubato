788ae85444f25cebc45919e5a13c430deca0934d
Kunal Chandan <kunal_chandan@outlook.com>
2026-02-02 05:29:39 -0500

    feat: harden Android Auto media service

---
+++ app/src/main/java/one/chandan/rubato/service/MediaService.kt
@@ -0,0 +1,232 @@
+package one.chandan.rubato.service
+
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.app.TaskStackBuilder
+import android.content.Intent
+import android.util.Log
+import androidx.media3.cast.CastPlayer
+import androidx.media3.cast.SessionAvailabilityListener
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.Tracks
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.DefaultLoadControl
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.session.MediaLibraryService
+import androidx.media3.session.MediaSession.ControllerInfo
+import one.chandan.rubato.BuildConfig
+import one.chandan.rubato.repository.AutomotiveRepository
+import one.chandan.rubato.ui.activity.MainActivity
+import one.chandan.rubato.util.Constants
+import one.chandan.rubato.util.DownloadUtil
+import one.chandan.rubato.util.Preferences
+import one.chandan.rubato.util.ReplayGainUtil
+import one.chandan.rubato.widget.WidgetUpdateHelper
+import com.google.android.gms.cast.framework.CastContext
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+
+@UnstableApi
+class MediaService : MediaLibraryService(), SessionAvailabilityListener {
+    private lateinit var automotiveRepository: AutomotiveRepository
+    private lateinit var player: ExoPlayer
+    private lateinit var castPlayer: CastPlayer
+    private lateinit var mediaLibrarySession: MediaLibrarySession
+    private val tag = "MediaService"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeRepository()
+        initializePlayer()
+        initializeCastPlayer()
+        initializeMediaLibrarySession()
+        initializePlayerListener()
+
+        setPlayer(
+                null,
+                if (this::castPlayer.isInitialized && castPlayer.isCastSessionAvailable) castPlayer else player
+        )
+
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, "onCreate initialized session")
+        }
+    }
+
+    override fun onGetSession(controllerInfo: ControllerInfo): MediaLibrarySession {
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, "onGetSession controller=${controllerInfo.packageName}")
+        }
+        return mediaLibrarySession
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        val player = mediaLibrarySession.player
+
+        if (!player.playWhenReady || player.mediaItemCount == 0) {
+            stopSelf()
+        }
+    }
+
+    override fun onDestroy() {
+        releasePlayer()
+        super.onDestroy()
+    }
+
+    private fun initializeRepository() {
+        automotiveRepository = AutomotiveRepository()
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(this)
+                .setRenderersFactory(getRenderersFactory())
+                .setMediaSourceFactory(getMediaSourceFactory())
+                .setAudioAttributes(AudioAttributes.DEFAULT, true)
+                .setHandleAudioBecomingNoisy(true)
+                .setWakeMode(C.WAKE_MODE_NETWORK)
+                .setLoadControl(initializeLoadControl())
+                .build()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun initializeCastPlayer() {
+        if (GoogleApiAvailability.getInstance()
+                        .isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS
+        ) {
+            castPlayer = CastPlayer(CastContext.getSharedInstance(this))
+            castPlayer.setSessionAvailabilityListener(this)
+        }
+    }
+
+    private fun initializeMediaLibrarySession() {
+        val sessionActivityPendingIntent =
+                TaskStackBuilder.create(this).run {
+                    addNextIntent(Intent(this@MediaService, MainActivity::class.java))
+                    getPendingIntent(0, FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT)
+                }
+
+        mediaLibrarySession =
+                MediaLibrarySession.Builder(this, player, createLibrarySessionCallback())
+                        .setSessionActivity(sessionActivityPendingIntent)
+                        .build()
+    }
+
+    private fun createLibrarySessionCallback(): MediaLibrarySession.Callback {
+        return MediaLibrarySessionCallback(this, automotiveRepository)
+    }
+
+    private fun initializePlayerListener() {
+        player.addListener(object : Player.Listener {
+            override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+                if (mediaItem == null) return
+
+                if (reason == Player.MEDIA_ITEM_TRANSITION_REASON_SEEK || reason == Player.MEDIA_ITEM_TRANSITION_REASON_AUTO) {
+                    MediaManager.setLastPlayedTimestamp(mediaItem)
+                }
+                WidgetUpdateHelper.requestUpdate(this@MediaService)
+            }
+
+            override fun onTracksChanged(tracks: Tracks) {
+                ReplayGainUtil.setReplayGain(player, tracks)
+                MediaManager.scrobble(player.currentMediaItem, false)
+
+                if (player.currentMediaItemIndex + 1 == player.mediaItemCount)
+                    MediaManager.continuousPlay(player.currentMediaItem)
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                if (BuildConfig.DEBUG) {
+                    Log.d(tag, "onIsPlayingChanged isPlaying=$isPlaying")
+                }
+                if (!isPlaying) {
+                    MediaManager.setPlayingPausedTimestamp(
+                            player.currentMediaItem,
+                            player.currentPosition
+                    )
+                } else {
+                    MediaManager.scrobble(player.currentMediaItem, false)
+                }
+                WidgetUpdateHelper.requestUpdate(this@MediaService)
+            }
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                if (BuildConfig.DEBUG) {
+                    Log.d(tag, "onPlaybackStateChanged state=$playbackState playWhenReady=${player.playWhenReady}")
+                }
+
+                if (!player.hasNextMediaItem() &&
+                        playbackState == Player.STATE_ENDED &&
+                        player.mediaMetadata.extras?.getString("type") == Constants.MEDIA_TYPE_MUSIC
+                ) {
+                    MediaManager.scrobble(player.currentMediaItem, true)
+                    MediaManager.saveChronology(player.currentMediaItem)
+                }
+                WidgetUpdateHelper.requestUpdate(this@MediaService)
+            }
+
+            override fun onPositionDiscontinuity(
+                    oldPosition: Player.PositionInfo,
+                    newPosition: Player.PositionInfo,
+                    reason: Int
+            ) {
+                super.onPositionDiscontinuity(oldPosition, newPosition, reason)
+
+                if (reason == Player.DISCONTINUITY_REASON_AUTO_TRANSITION) {
+                    if (oldPosition.mediaItem?.mediaMetadata?.extras?.getString("type") == Constants.MEDIA_TYPE_MUSIC) {
+                        MediaManager.scrobble(oldPosition.mediaItem, true)
+                        MediaManager.saveChronology(oldPosition.mediaItem)
+                    }
+
+                    if (newPosition.mediaItem?.mediaMetadata?.extras?.getString("type") == Constants.MEDIA_TYPE_MUSIC) {
+                        MediaManager.setLastPlayedTimestamp(newPosition.mediaItem)
+                    }
+                }
+                WidgetUpdateHelper.requestUpdate(this@MediaService)
+            }
+        })
+    }
+
+    private fun initializeLoadControl(): DefaultLoadControl {
+        return DefaultLoadControl.Builder()
+                .setBufferDurationsMs(
+                        (DefaultLoadControl.DEFAULT_MIN_BUFFER_MS * Preferences.getBufferingStrategy()).toInt(),
+                        (DefaultLoadControl.DEFAULT_MAX_BUFFER_MS * Preferences.getBufferingStrategy()).toInt(),
+                        DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS,
+                        DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS
+                )
+                .build()
+    }
+
+    private fun setPlayer(oldPlayer: Player?, newPlayer: Player) {
+        if (oldPlayer === newPlayer) return
+        oldPlayer?.stop()
+        mediaLibrarySession.player = newPlayer
+    }
+
+    private fun releasePlayer() {
+        if (this::castPlayer.isInitialized) castPlayer.setSessionAvailabilityListener(null)
+        if (this::castPlayer.isInitialized) castPlayer.release()
+        player.release()
+        mediaLibrarySession.release()
+        automotiveRepository.deleteMetadata()
+        clearListener()
+    }
+
+    private fun getRenderersFactory() = DownloadUtil.buildRenderersFactory(this, false)
+
+    private fun getMediaSourceFactory() =
+            DefaultMediaSourceFactory(this).setDataSourceFactory(DownloadUtil.getDataSourceFactory(this))
+
+    override fun onCastSessionAvailable() {
+        setPlayer(player, castPlayer)
+    }
+
+    override fun onCastSessionUnavailable() {
+        setPlayer(castPlayer, player)
+    }
+}
